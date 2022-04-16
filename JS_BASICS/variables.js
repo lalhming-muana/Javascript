08ea5a6b848c8585d1db738f58b20a68976b94d2
lhmuana <edd_vai@yahoo.co.in>
2022-04-16 22:09:06 +0530

    variable.js file updated

---
+++ JS_BASICS/variables.js
@@ -0,0 +1,6 @@
+var george = " this is a new string" + "!";
+console.log(george);
+
+//  variables can start with $ and _
+//  prompt() works in browser because it is a method of the window object
+//  which works only in browsers.
