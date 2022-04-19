3893b853176ad137d9f769b80ce82415b670cf80
lhmuana <edd_vai@yahoo.co.in>
2022-04-19 19:58:40 +0530

    if statement added

--- JS_BASICS/control_flow.js
+++ JS_BASICS/control_flow.js
@@ -1,7 +1,14 @@
 
-
-
 // for loop
 for(let i=0; i<10;i++){
     console.log(i);
+}
+
+var myName = "Billy";
+if(myName === "Billy"){
+    console.log("Hi billy");
+}
+
+if(myName !== "Sussan"){
+    console.log("This is not Billy");
 }
